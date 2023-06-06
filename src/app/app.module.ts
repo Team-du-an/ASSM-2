fc4842c6b2d6775369035cefe3944efddb345baf
Ho Van Thang <106424819+anhthang111@users.noreply.github.com>
2023-06-06 22:26:20 +0700

    update about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AboutpageComponent
   ],
   imports: [
     BrowserModule,

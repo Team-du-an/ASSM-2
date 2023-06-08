929a8a7a16a005b998d83243eb4a7320f3c81195
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2023-06-09 00:11:59 +0700

    đã ghép code feat:SPW5&SPW9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { RegisterComponent } from './pages/admin/register/register.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { AdminGuard } from './middlewares/auth.guard';
+import { ListUserComponent } from './pages/admin/list-user/list-user.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { CreateProductComponent } from './pages/admin/create-product/create-product.component';
+import { ListProductComponent } from './pages/admin/list-product/list-product.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'admin',
+        component: BaseLayoutComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full',
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
+                canActivate: [AdminGuard],
+            },
+            {
+                path: 'register',
+                component: RegisterComponent,
+            },
+            {
+                path: 'login',
+                component: LoginComponent,
+            },
+            {
+                path: 'list-user',
+                component: ListUserComponent,
+                canActivate: [AdminGuard],
+            },
+            {
+                path: 'create-product',
+                component: CreateProductComponent,
+            },
+            {
+                path: 'list-product',
+                component: ListProductComponent,
+            },
+        ],
+    },
+
+    { path: 'signin', component: SigninComponent },
+    { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxDropzoneModule } from 'ngx-dropzone';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MaterialModule } from 'src/material.module';
+import { RegisterComponent } from './pages/admin/register/register.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { ListUserComponent } from './pages/admin/list-user/list-user.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { CreateProductComponent } from './pages/admin/create-product/create-product.component';
+import { ListProductComponent } from './pages/admin/list-product/list-product.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { ModelDeleteProductComponent } from './components/model-delete-product/model-delete-product.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
-    declarations: [AppComponent, HomePageComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    declarations: [
+        AppComponent,
+        RegisterComponent,
+        LoginComponent,
+        BaseLayoutComponent,
+        ListUserComponent,
+        UpdateUserComponent,
+        DashboardComponent,
+        CreateProductComponent,
+        ListProductComponent,
+        UpdateProductComponent,
+        ModelDeleteProductComponent,
+        SigninComponent,
+        SignupComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MaterialModule,
+        NgxDropzoneModule,
+
+        ToastrModule.forRoot(),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })

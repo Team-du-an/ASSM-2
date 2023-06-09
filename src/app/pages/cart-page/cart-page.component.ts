22ed3710360856f7141365d6535de4a09a3c3bde
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2023-06-09 20:16:23 +0700

    feat:CRUD fix bug toastr

---
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+    selector: 'app-cart-page',
+    templateUrl: './cart-page.component.html',
+    styleUrls: ['./cart-page.component.scss'],
+})
+export class CartPageComponent implements OnInit {
+    public products: any = [];
+    public grandTotal: number = 0;
+
+    constructor(private cartService: CartService) {}
+
+    ngOnInit(): void {
+        this.cartService.getProduct().subscribe((res) => {
+            this.products = res;
+
+            this.grandTotal = this.cartService.getTotalPrice();
+        });
+    }
+
+    removeCartItem(item: any) {
+        this.cartService.removeCartItem(item);
+    }
+
+    emptyCart() {
+        this.cartService.removeAllCart();
+    }
+}

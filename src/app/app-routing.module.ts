c18b758293ac79f73944de3d1736d30d833e5fe9
Ho Van Thang <106424819+anhthang111@users.noreply.github.com>
2023-06-10 10:24:00 +0700

    homepgae

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './Layouts/base-layout/base-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path : " ", component : BaseLayoutComponent , children:[
+  {path : " " , component : HomepageComponent}
+]}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,6 +8,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { AboutpageComponent } from './aboutpage/aboutpage.component';
 import { ProductlistComponent } from './components/productlist/productlist.component';
 import { ModalComponent } from './components/modal/modal.component';
+
 
 @NgModule({
   declarations: [

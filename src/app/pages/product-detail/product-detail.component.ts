95328befe42e3b73f9aa9f181a4238df22cd4686
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2023-06-12 23:54:01 +0700

    feat: create logic detail

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+import { IProduct } from 'src/interfaces/Product';
 
 @Component({
     selector: 'app-product-detail',
     templateUrl: './product-detail.component.html',
     styleUrls: ['./product-detail.component.scss'],
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent implements OnInit {
+    product!: IProduct;
+
+    constructor(
+        private ptoductService: ProductService,
+        private route: ActivatedRoute,
+        private toastr: ToastrService,
+        private cartService: CartService,
+    ) {}
+
+    ngOnInit(): void {
+        this.route.paramMap.subscribe((params) => {
+            const id = Number(params.get('id'));
+
+            this.ptoductService.getProductById(id).subscribe((res) => {
+                this.product = res;
+            });
+        });
+    }
+
+    addToCart(item: any) {
+        this.toastr.success('Bạn đã thêm sản phẩm vào giỏ hàng', 'Mua thêm đi 😁😁');
+        this.cartService.addToCart(item);
+    }
+}
